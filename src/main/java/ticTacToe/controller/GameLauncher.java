db5cb5f8a2317f8431e64d85d96b7d279b2eb980
Atharva-Shelke <atharva.a.s.00@gmail.com>
2026-05-24 14:57:03 +0530

    Added controller

---
+++ src/main/java/ticTacToe/controller/GameLauncher.java
@@ -0,0 +1,11 @@
+package ticTacToe.controller;
+
+import javafx.application.Application;
+import ticTacToe.view.TicTacToeUI;
+
+public class GameLauncher {
+
+	public static void main(String[] args) {
+		Application.launch(TicTacToeUI.class);
+	}
+}
